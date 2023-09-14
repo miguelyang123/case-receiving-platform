5bb5ab9d0c343854041c70871db402bbcf2db235
Miguel Yang <miguelyang1@gmail.com>
2023-09-14 16:32:41 +0800

    Delete EnableAuto

--- src/main/java/team/bool/case_receiving_platform/CaseReceivingPlatformApplication.java
+++ src/main/java/team/bool/case_receiving_platform/CaseReceivingPlatformApplication.java
@@ -1,12 +1,9 @@
 package team.bool.case_receiving_platform;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class CaseReceivingPlatformApplication {
 
