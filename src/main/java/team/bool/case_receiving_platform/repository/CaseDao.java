d2d91e2519e52504aade934751e63ff5403cfe61
Miguel Yang <miguelyang1@gmail.com>
2023-09-14 17:46:34 +0800

    Fix CaseDao

--- src/main/java/team/bool/case_receiving_platform/repository/CaseDao.java
+++ src/main/java/team/bool/case_receiving_platform/repository/CaseDao.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.bool.case_receiving_platform.entity.Case;
 
 
-public interface CaseDao extends JpaRepository<Case, String>{
+public interface CaseDao extends JpaRepository<Case, Integer>{
 
 }
