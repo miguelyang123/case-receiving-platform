f4674c362922c290843808b5b6f6ee1f980e383e
Miguel Yang <miguelyang1@gmail.com>
2023-09-14 17:45:56 +0800

    fix Dao

--- src/main/java/team/bool/case_receiving_platform/repository/UserDao.java
+++ src/main/java/team/bool/case_receiving_platform/repository/UserDao.java
@@ -1,6 +1,7 @@
 package team.bool.case_receiving_platform.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import team.bool.case_receiving_platform.entity.User;
 
 @Repository
-public interface UserDao extends JpaRepository<User, String>{
+public interface UserDao extends JpaRepository<User, UUID>{
 	
 	public Optional<User> findByEmail(String email);
 }
