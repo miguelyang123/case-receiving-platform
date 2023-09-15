b2acaf496d2918bf2446872b691635eaec1b7cde
Miguel Yang <miguelyang1@gmail.com>
2023-09-15 09:59:35 +0800

    Update UserService

--- src/main/java/team/bool/case_receiving_platform/service/ifs/UserService.java
+++ src/main/java/team/bool/case_receiving_platform/service/ifs/UserService.java
@@ -1,5 +1,6 @@
 package team.bool.case_receiving_platform.service.ifs;
 
+import team.bool.case_receiving_platform.entity.User;
 import team.bool.case_receiving_platform.vo.AuthRes;
 
 public interface UserService {
@@ -7,5 +8,7 @@ public interface UserService {
 	public AuthRes login(String email, String pwd);
 	
 	public AuthRes getBalance(String email, String pwd);
+	
+	public AuthRes addNewUser(User user);
 	
 }
--- src/main/java/team/bool/case_receiving_platform/service/impl/UserServiceImpl.java
+++ src/main/java/team/bool/case_receiving_platform/service/impl/UserServiceImpl.java
@@ -20,53 +20,37 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserDao userDao;
-	
+
 	private String emailPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
 
-	@Override
-	public AuthRes login(String email, String pwd) {
-
-		return checkUserEmailAndPwd(email, pwd);
-	}
-
-	@Override
-	public AuthRes getBalance(String email, String pwd) {
-
-		return checkUserEmailAndPwd(email, pwd);
-	}
-
 	// check Email And Pwd at DB
-	private AuthRes checkUserEmailAndPwd(String email, String pwd) {
-		
+	private AuthRes checkUserEmailAndPwdAtDB(String email, String pwd) {
+
 		// input is null & check input format
 		if (!StringUtils.hasText(email) || !email.matches(emailPattern)) {
-			return new AuthRes(AuthRtnCode.EMAIL_FORMAT_ERROR.getCode(),
-					AuthRtnCode.EMAIL_FORMAT_ERROR.getMessage());
+			return new AuthRes(AuthRtnCode.EMAIL_FORMAT_ERROR.getCode(), AuthRtnCode.EMAIL_FORMAT_ERROR.getMessage());
 		}
 		if (!StringUtils.hasText(pwd) || pwd.length() > 30 || pwd.length() <= 8) {
-			return new AuthRes(AuthRtnCode.PASSWORD_FORMAT_ERROR.getCode(),
-					AuthRtnCode.PASSWORD_FORMAT_ERROR.getMessage());
+			return new AuthRes(AuthRtnCode.WORONG_PASSWORD.getCode(),
+					AuthRtnCode.WORONG_PASSWORD.getMessage());
 		}
 
 		Optional<User> op = userDao.findByEmail(email);
 
 		// Account not found
 		if (op.isEmpty()) {
-			return new AuthRes(AuthRtnCode.ACCOUNT_NOT_FOUND.getCode(),
-					AuthRtnCode.ACCOUNT_NOT_FOUND.getMessage());
+			return new AuthRes(AuthRtnCode.ACCOUNT_NOT_FOUND.getCode(), AuthRtnCode.ACCOUNT_NOT_FOUND.getMessage());
 		}
 		// check pwd
 		if (!op.get().getPwd().equals(pwd)) {
-			return new AuthRes(AuthRtnCode.WORONG_PASSWORD.getCode(),
-					AuthRtnCode.WORONG_PASSWORD.getMessage());
+			return new AuthRes(AuthRtnCode.WORONG_PASSWORD.getCode(), AuthRtnCode.WORONG_PASSWORD.getMessage());
 		}
 
 		// change User to UserInfo
 		try {
 			// Successful get UserInfo
 			UserInfo userInfo = userToUserInfo(op.get());
-			return new AuthRes(AuthRtnCode.SUCCESSFUL.getCode(), AuthRtnCode.SUCCESSFUL.getMessage(),
-					userInfo);
+			return new AuthRes(AuthRtnCode.SUCCESSFUL.getCode(), AuthRtnCode.SUCCESSFUL.getMessage(), userInfo);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -76,6 +60,33 @@ public class UserServiceImpl implements UserService {
 
 	}
 
+	// ch
+//	private AuthRes checkUserInput(User user) {
+//
+//		String email = user.getEmail();
+//		String pwd = user.getPwd();
+//		String userName = user.getUserName();
+//		String phone = user.getPhone();
+//		
+//		
+//		// input is null & check input format
+//		if (!StringUtils.hasText(email) || !email.matches(emailPattern)) {
+//			return new AuthRes(AuthRtnCode.EMAIL_FORMAT_ERROR.getCode(), AuthRtnCode.EMAIL_FORMAT_ERROR.getMessage());
+//		}
+//		if (!StringUtils.hasText(pwd) || pwd.length() > 30 || pwd.length() <= 8) {
+//			return new AuthRes(AuthRtnCode.PASSWORD_FORMAT_ERROR.getCode(),
+//					AuthRtnCode.PASSWORD_FORMAT_ERROR.getMessage());
+//		}
+//		
+//		
+//
+//
+//		return new AuthRes(AuthRtnCode.SUCCESSFUL.getCode(), AuthRtnCode.SUCCESSFUL.getMessage(),null);
+//
+//		
+//
+//	}
+	
 	private UserInfo userToUserInfo(User user) throws Exception {
 		ObjectMapper objectMapper = new ObjectMapper();
 
@@ -83,5 +94,27 @@ public class UserServiceImpl implements UserService {
 		String json = objectMapper.writeValueAsString(user);
 		return objectMapper.readValue(json, UserInfo.class);
 	}
+
+	
+	
+	@Override
+	public AuthRes login(String email, String pwd) {
+
+		return checkUserEmailAndPwdAtDB(email, pwd);
+	}
+
+	@Override
+	public AuthRes getBalance(String email, String pwd) {
+
+		return checkUserEmailAndPwdAtDB(email, pwd);
+	}
+
+	@Override
+	public AuthRes addNewUser(User user) {
+		
+		return null;
+	}
+	
+	
 
 }
