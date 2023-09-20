29abc18d09aba7f514029f019ef9191d2e68e8fc
Miguel Yang <miguelyang1@gmail.com>
2023-09-20 16:46:15 +0800

    Change search user post to get

--- src/main/java/team/bool/case_receiving_platform/controller/SearchUserController.java
+++ src/main/java/team/bool/case_receiving_platform/controller/SearchUserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import team.bool.case_receiving_platform.service.ifs.SearchUserService;
@@ -23,10 +24,15 @@ public class SearchUserController {
 		return sService.findAllUser();
 	}
 
-	@PostMapping("with_Input")
-	public UserListRes searchUserWithInput(@RequestBody SearchUserReq req) {
-		return sService.searchUserToList(req.getUserName(), req.getMinRating(), req.getIsAdministrator(),
-				req.getLockedStatus());
+	@GetMapping("with_Input")
+	public UserListRes searchUserWithInput(
+			@RequestParam(name = "name", required = false) String userName,
+			@RequestParam(name = "rating", required = false) Integer minRating,
+			@RequestParam(name = "admin", required = false) Boolean isAdministrator,
+			@RequestParam(name = "locked", required = false) Boolean lockedStatus
+			) {
+		
+		return sService.searchUserToList(userName, minRating, isAdministrator, lockedStatus);
 	}
 
 }
